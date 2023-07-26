bb5283bbac3700e48e3109da607be68e441a0970
unknown <maksimiak.lukas@gmail.com>
2023-07-26 15:59:40 +0300

    Fixed the confusion

--- random3sat/main.cpp
+++ random3sat/main.cpp
@@ -4,8 +4,12 @@
 #include <vector>
 #include <math.h>
 #include <time.h>
+#include <map>
 
 using namespace std;
+
+constexpr const size_t MAX_SAT = 3;
+constexpr const size_t NO_VARS = 4;
 
 typedef vector<bool> Assignment;
 
@@ -20,16 +24,16 @@ f = (p v q v r) & (q v r v l) & (r v q v p)
 
 */
 
-
-vector<bool> vars(4);
-
+vector<bool> vars(NO_VARS);
 
 class Clause
 {
 public:
-    Clause(vector<bool>& literas) : m_literas(literas), m_value(false) {}
+    Clause(vector<size_t> indexes, vector<size_t> neg_indexes = vector<size_t>())
+    : m_indexes(indexes), m_neg_indexes(neg_indexes), m_value(false) {}
 
-    vector<bool>& m_literas;
+    vector<size_t> m_indexes;
+    vector<size_t> m_neg_indexes;
     bool m_value;
 };
 
@@ -40,29 +44,31 @@ public:
 
     bool evaluate()
     {
+
         bool result = true;
 
         // evaluate each clause and function
         for (size_t index = 0; index < m_clauses.size(); ++index)
         {
             // go through each clause
-            for (size_t cl_index = 0; cl_index < m_clauses[index].m_literas.size(); ++cl_index)
+            for (size_t cl_index = 0; cl_index < m_clauses[index].m_indexes.size(); ++cl_index)
             {
-                m_clauses[index].m_value |= m_clauses[index].m_literas[cl_index];
+                m_clauses[index].m_value |= vars[m_clauses[index].m_indexes[cl_index]];
             }
             result &= m_clauses[index].m_value;
         }
         return result;
+
+        return false;
     }
 
     void display_interpretation()
     {
         for (size_t index = 0; index < m_clauses.size(); ++index)
         {
-            // go through each clause
-            for (size_t cl_index = 0; cl_index < m_clauses[index].m_literas.size(); ++cl_index)
+            for (size_t cl_index = 0; cl_index < m_clauses[index].m_indexes.size(); ++cl_index)
             {
-                cout << m_clauses[index].m_literas[cl_index] << " ";
+                cout << vars[m_clauses[index].m_indexes[cl_index]] << " ";
             }
             cout << endl;
         }
@@ -85,14 +91,13 @@ public:
     vector<Clause> m_clauses;
 };
 
-
 bool random_walk(Assignment a, Function& func)
 {
     //  a' = a;
     vars = a;
 
     size_t n = vars.size();
-    size_t times = 3 * n;
+    size_t times = MAX_SAT * n;
 
     for (size_t index = 0; index < times; ++index)
     {
@@ -106,51 +111,42 @@ bool random_walk(Assignment a, Function& func)
         }
         // C - clause that is not satisfied by temp_a
         size_t unsatisfied = func.un_idx();
-        size_t urandom = (rand() % 3) + 1;
-        // flip
-        func.m_clauses[unsatisfied].m_literas[urandom] = !func.m_clauses[unsatisfied].m_literas[urandom];
+        size_t urandom = rand() % MAX_SAT;
 
+
+        // flip in all clauses
+        size_t var_idx = func.m_clauses[unsatisfied].m_indexes[urandom];
+        vars[var_idx] = !vars[var_idx];
     }
 }
 
-
-
 int main()
 {
+
     srand(time(0));
+    // todo: what about negated variables
+    // add another vector thats empty by default as negation list
 
-    Clause c1(vars);
-    c1.m_literas = vector<bool>{vars[0], vars[1], vars[2]};
-    Clause c2(vars);
-    c2.m_literas = vector<bool>{vars[0], vars[3], vars[1]};
+    Clause c0({2, 1, 1}); // initialized with index of available variables
+    Clause c01({3, 3, 3});
+    Clause c02({2, 0, 0});
 
-
-    vector<Clause> clauses {c1, c2};
+    vector<Clause> clauses {c0, c01, c02};
 
     /**
-    f = (x1 v x2 v x3) & (x1 v x4 v x2)
+    f = (x3 v x2 v x2) & (x4 v x4 v x4) & (x3 v x1 v x1)
     */
 
     Function func1(clauses);
-    // cout << func1.evaluate() << endl;
+    func1.display_interpretation();
+    // func1.evaluate();
 
-
-    Assignment a1 {
-    {
-        // should satisfy
-        false, false, false, false,
-    }
-    };
-
+    Assignment a {false, false, false, false};
 
     for (;;)
     {
-        if (random_walk(a1, func1))
-        {
-            break;
-        }
+        if (random_walk(a, func1)) break;
     }
-
 
     return 0;
 }
